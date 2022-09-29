1af3141c36f2409e005544bcbb9c41f1a84603f9
sathninduk <sathnidukottage@gmail.com>
2022-09-29 19:44:44 +0530

    apollo

---
+++ apollo/type-defs.js
@@ -0,0 +1,38 @@
+import { gql } from '@apollo/client'
+// const { gql } = require('apollo-server');
+
+export const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    status: String!
+  }
+  
+  
+  
+  
+  
+  input UserInput {
+    name: String!
+    status: String!
+  }
+  
+  input EditUserInput {
+    name: String
+  }
+  
+  
+  
+  
+
+  type Query {
+    viewer(ID: ID!): User!
+    getUsers: [User]
+  }
+  
+  type Mutation {
+    createUser(userInput: UserInput): User!
+    deleteUser(ID: ID!): Boolean
+    editUser(ID: ID!, editUserInput: EditUserInput): Boolean
+  }
+`
